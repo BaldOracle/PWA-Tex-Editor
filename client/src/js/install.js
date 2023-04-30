f233f6f12e60d7ffdf98d2ee8ff4a21ee91d68f1
Mr Milk Man <sdmurphy861@gmail.com>
2023-04-30 12:30:26 -0400

    updated the todos

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,43 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    const deferredPrompt = event;
+    const installButton = document.getElementById('install-button');
+    installButton.style.display = 'block';
+    installButton.addEventListener('click', () => {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the PWA installation');
+            } else {
+                console.log('User declined the PWA installation');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if ('serviceWorker' in navigator) {
+        const registration = await navigator.serviceWorker.register('/sw.js');
+        console.log('Service worker registered:', registration);
+        if ('BeforeInstallPromptEvent' in window) {
+            const event = new BeforeInstallPromptEvent('beforeinstallprompt');
+            window.dispatchEvent(event);
+        } else {
+            console.log('The BeforeInstallPromptEvent is not supported');
+        }
+    } else {
+        console.log('Service workers are not supported');
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('PWA installed successfully!');
+});
+
